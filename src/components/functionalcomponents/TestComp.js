8665dd4ad4300ae66bb14f813774a21c14a56088
sameermgit <sammunaf812@gmail.com>
2024-01-25 10:07:37 +0530

    v1

---
+++ src/components/functionalcomponents/TestComp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function TestComp(props){
+    return(
+        <div>
+            <h1>This is {props.name}</h1>
+        </div>
+    );
+}
+export default TestComp;
+//const fC= ReactDOM.createRoot(document.getElementById('root'))
+//fC.render(
+//    <TestComp/>
+//)
